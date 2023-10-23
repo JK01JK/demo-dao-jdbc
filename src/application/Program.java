0f1507a70e0e763d626056371ff1c41bf1729a12
Juliano K <julianojk01@gmail.com>
2023-10-23 18:15:43 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,13 @@ public class Program {
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		//System.out.println(obj);
 		//System.out.println(seller);
+		
+		System.out.println("\n=== TEST 5: seller update ======");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		System.out.println(seller);		
 
 	}
 
